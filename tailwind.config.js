5d1cbc0d732729156046d993e0f2a447c370ca6f
theyashpatel <yashpatel26@gmail.com>
2022-06-19 23:38:10 +0530

    a

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        lightColor: "#04DFBE",
+        darkColor: "#2D474E",
+      },
+    },
+  },
+  plugins: [],
+};
